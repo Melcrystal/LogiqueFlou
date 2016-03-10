884c40f589a50c100c3760e30e56e246bbc5b9a3
Melcrystal <yomitsuki3.0@gmail.com>
2016-03-10 08:45:24 +0100

    voili

--- NaryExpressionModel.h
+++ NaryExpressionModel.h
@@ -1,1 +1,19 @@
+#ifndef NARYEXPRESSION_H
+#define NARYEXPRESSION_H
 
+#include"NaryExpression.h"
+#include"Expression.h"
+
+http://fr.nametests.com/test/result/guilhem-/10716862130/namespace core
+{
+    template <class T>
+	class NaryExpressionModel :public Expression<T>, public NaryExpression<T>
+	{
+    public :
+        NaryExpressionModel(NaryExpression<T>, std::vector<Expression<T>>);
+        virtual ~NaryExpressionModel(){};
+
+        T evaluate() const;
+        T evaluate(vector<const Expression<T>*>*) const;
+	}
+}
